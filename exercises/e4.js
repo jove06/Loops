a529417c4f8cec9b97fa12d751d61c3acb749570
Alexia Riccardi <alexiariccardi@gmail.com>
2024-08-06 23:22:19 -0400

    solved e4

--- exercises/e4.js
+++ exercises/e4.js
@@ -4,12 +4,28 @@
 // findMaxValueInArray([2, 45, 32, 3, 0, 46, 12]) => 46
 
 export function findMinValueInArray(array) {
-  // Your code goes here...
+  let min = array[0];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] < min){
+      min = array[i];
+    }
+  }
+return min;
 }
 
+console.log(findMinValueInArray([2, 45, 0, 13, 54, 17]));
+
 export function findMaxValueInArray(array) {
-  // Your code goes here...
+  let max = array[0];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] > max){
+      max = array[i];
+    }
+  }
+return max;
 }
+
+console.log(findMaxValueInArray([2, 45, 0, 13, 54, 17]));
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-4"
