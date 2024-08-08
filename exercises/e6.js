3a4e9b580600977ba15d07e28596545ae283057c
Alexia Riccardi <alexiariccardi@gmail.com>
2024-08-07 23:00:36 -0400

    solved e6

--- exercises/e6.js
+++ exercises/e6.js
@@ -3,11 +3,40 @@
 // Array example: bankAccounts in /data/data.js
 // getClientWithNoMoney(bankAccounts) => ['Kevin', 'Jon']
 
-export function getClientWithNoMoney(array) {
-  // Your code goes here...
 
+const bankAccounts = [
+  {id: 1,
+    name: "Jason",
+    balance: 230,
+  },
+
+  {id: 2,
+    name: "Jack",
+    balance: 0,
+  },
+
+  {id: 3,
+    name: "Jon",
+    balance: 240,
+  },
+
+  {id: 4,
+    name: "Jasper",
+    balance: 0,
+  }
+]
+
+export function getClientWithNoMoney(array) {
+  let nameBalance = [];
+  for (let i = 0; i < array.length; i++){
+    if (array[i].balance === 0){
+      nameBalance.push(array[i].name);
+    }
+  }
+  return nameBalance;
 }
 
+console.log(getClientWithNoMoney(bankAccounts));
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-6"
 // If the test has all tests passed, switch to the next exercise file
