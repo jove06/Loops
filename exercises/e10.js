3a4a17efe6bb62fa2eb788b8ffe80cc78d119a97
Alexia Riccardi <alexiariccardi@gmail.com>
2024-08-10 14:04:23 -0400

    solved e8, e9, e10

--- exercises/e10.js
+++ exercises/e10.js
@@ -3,10 +3,40 @@
 // Array example: bankAccounts in /data/data.js
 // getClientsWithLetterInName(bankAccounts, 'e') => ['Kevin', 'Steven', ...]
 
+const bankAccounts = [
+  {
+    id: 1,
+    name: "Susan",
+    balance: 100.32,
+    deposits: [150, 30, 221],
+    withdrawals: [110, 70.68, 120],
+  },
+  { id: 2, name: "Morgan", balance: 1100.0, deposits: [1100] },
+  {
+    id: 3,
+    name: "Joshua",
+    balance: 18456.57,
+    deposits: [4000, 5000, 6000, 9200, 256.57],
+    withdrawals: [1500, 1400, 1500, 1500],
+  },
+  { id: 4, name: "Candy", balance: 0.0 },
+  { id: 5, name: "Phil", balance: 18, deposits: [100, 18], withdrawals: [100] },
+];
+
 export function getClientsWithLetterInName(array, letter) {
-  // Your code goes here...
+  let result = [];
+  for (let i = 0; i < array.length; i++){
+    if (array[i].name.toLowerCase().includes(letter)){
+      result.push(array[i].name);
+
+    }
+    
+  }
+  return result;
 
 }
+
+console.log(getClientsWithLetterInName(bankAccounts, 'c'));
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-10"
