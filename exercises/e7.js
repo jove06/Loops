d6b38e3bfe3389edeb6bfaa7a84dfb662c95e1b2
Alexia Riccardi <alexiariccardi@gmail.com>
2024-08-10 10:59:02 -0400

    solved e7

--- exercises/e7.js
+++ exercises/e7.js
@@ -4,10 +4,44 @@
 // Array example: bankAccounts in /data/data.js
 // getClientWithLeastBalance(bankAccounts) => [{ name: 'SomeName', balance: 32, ... }]
 
+const bankAccounts = [
+  {
+    id: 1,
+    name: "Susan",
+    balance: 100.32,
+    deposits: [150, 30, 221],
+    withdrawals: [110, 70.68, 120],
+  },
+  { id: 2, name: "Morgan", balance: 1100.0, deposits: [1100] },
+  {
+    id: 3,
+    name: "Joshua",
+    balance: 18456.57,
+    deposits: [4000, 5000, 6000, 9200, 256.57],
+    withdrawals: [1500, 1400, 1500, 1500],
+  },
+  { id: 4, name: "Candy", balance: 0.0 },
+  { id: 5, name: "Phil", balance: 18, deposits: [100, 18], withdrawals: [100] },
+];
+
 export function getClientWithLeastPositiveBalance(array) {
-  // Your code goes here...
+  let min = null;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].balance > 0){
+      min = array[i];
+    } 
+  }
+
+  if (min === null) {
+    return [];
+  } else {return [min];}
   
 }
+  
+
+console.log(getClientWithLeastPositiveBalance(bankAccounts));
+
+
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-7"
